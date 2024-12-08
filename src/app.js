9ed412582de156aad6373a1cd531bbd83ca30974
LucasAguiarUtec1 <111097848+LucasAguiarUtec1@users.noreply.github.com>
2024-12-08 15:40:47 +0000

    proyecto excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoSelected, actionSelected, whatSelected, whenSelected;
+
+  whoSelected = who[Math.floor(Math.random() * who.length)];
+
+  actionSelected = action[Math.floor(Math.random() * action.length)];
+
+  whatSelected = what[Math.floor(Math.random * what.length)];
+
+  whenSelected = when[Math.floor(Math.random() * when.length)];
+
+  let element = document.querySelector("#excuse");
+  element.innerHTML =
+    whoSelected +
+    " " +
+    actionSelected +
+    " " +
+    whatSelected +
+    " " +
+    whenSelected;
+
+  console.log("selected: " + whoSelected);
+
   console.log("Hello Rigo from the console!");
 };
